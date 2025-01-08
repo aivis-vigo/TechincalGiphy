2a58067dec1d8c7c25c30782d9090d72cb92becf
aivis-vigo <aivisvigoreimarts@gmail.com>
2025-01-08 18:30:33 +0200

    Initial Commit

---
+++ Giphy/GiphyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiphyApp.swift
+//  Giphy
+//
+//  Created by Aivis Vigo Reimarts on 08/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct GiphyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GiphyTests/GiphyTests.swift
@@ -0,0 +1,17 @@
+//
+//  GiphyTests.swift
+//  GiphyTests
+//
+//  Created by Aivis Vigo Reimarts on 08/01/2025.
+//
+
+import Testing
+@testable import Giphy
+
+struct GiphyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
